85e62a23569a47777457ca81e2192d42ff0170e5
Aditya Pratap Singh <adityapratapsjnhh7654@gmail.com>
2023-10-28 18:43:13 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.multiple-texts', {
+    strings: ['FrontEnd Web Developer', 'Youtuber', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    baclDelay: 1000,
+    loop: true
+})
+
+const title = document.getElementById('title');
+console.log(title);
